ea36c09672b824ca397a413bfaf2b469d205cbba
Mahendra Babu <mahiramisetty141@gmail.com>
2025-08-06 16:04:27 -0400

    small change

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return f"Hello this is {os.getenv('USER_NAME', 'Mahendra')}!"
+    return f"Hello this is nn{os.getenv('USER_NAME', 'Mahendra')}!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.getenv("PORT", 5000)))
